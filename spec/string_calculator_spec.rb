a97f3239f892c8502120123aa35bf23e18f04038
Neha <nehac@vueops.com>
2025-05-21 22:12:23 +0530

    Fail: Add tests for negative number exception

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,17 @@ RSpec.describe StringCalculator do
     it 'supports custom delimiter in format //;\n1;2' do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
+
+    it 'raises an exception for negative numbers' do
+      expect {
+        StringCalculator.add("1,-2,3")
+      }.to raise_error("negative numbers not allowed: -2")
+    end
+    
+    it 'lists all negative numbers in exception message' do
+      expect {
+        StringCalculator.add("1,-2,-3,4")
+      }.to raise_error("negative numbers not allowed: -2, -3")
+    end
   end
 end
