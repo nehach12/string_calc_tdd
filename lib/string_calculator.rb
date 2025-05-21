19476f00ad4af5efad40b6e681b211821904ca00
Neha <nehac@vueops.com>
2025-05-21 22:11:58 +0530

    Pass: Support custom delimiter format

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,14 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.match(%r{//(.)\n(.*)}).captures
+      delimiter_regex = Regexp.escape(delimiter)
+    else
+      delimiter_regex = ",|\n"
+    end
+
+    numbers.split(/#{delimiter_regex}/).map(&:to_i).sum
   end
 end
