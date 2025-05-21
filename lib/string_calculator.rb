7e044206da847b26cd332a088adb1a5cafaba74b
Neha <nehac@vueops.com>
2025-05-21 22:12:51 +0530

    Pass: Raise exception for negative numbers with message

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,6 +9,12 @@ class StringCalculator
       delimiter_regex = ",|\n"
     end
 
-    numbers.split(/#{delimiter_regex}/).map(&:to_i).sum
+    nums = numbers.split(/#{delimiter_regex}/).map(&:to_i)
+    negatives = nums.select { |n| n < 0 }
+    if negatives.any?
+      raise "negative numbers not allowed: #{negatives.join(', ')}"
+    end
+
+    nums.sum
   end
 end
