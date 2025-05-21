fdd59daa90250db0d128ff485f1923a0011624fc
Neha <nehac@vueops.com>
2025-05-21 22:10:48 +0530

    Fail: Add test for newline delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two comma-separated numbers' do
       expect(StringCalculator.add("1,2")).to eq(3)
     end
+
+    it 'supports newlines as delimiters' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
