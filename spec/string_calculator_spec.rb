b0057f854f004ed8636274b13378a4890c1cdc01
Neha <nehac@vueops.com>
2025-05-21 22:08:04 +0530

    Fail: Add test for single number input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns number itself when only one number is provided' do
+      expect(StringCalculator.add("4")).to eq(4)
+    end
   end
 end
