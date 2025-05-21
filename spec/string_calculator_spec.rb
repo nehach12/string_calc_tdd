998142434a33e6686024d4f799b61e37a0fc440a
Neha <nehac@vueops.com>
2025-05-21 22:11:32 +0530

    Fail: Add test for custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator do
     it 'supports newlines as delimiters' do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom delimiter in format //;\n1;2' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
